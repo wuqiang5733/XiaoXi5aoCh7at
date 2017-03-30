bd56417c016b5e0f96176a8fdf45fd81680a56df
xiaoqianghou <2662651103@qq.com>
2017-03-30 17:50:07 +0800

    加上了9png图片_可以分左右了

--- app/src/main/java/org/xuxiaoxiao/www/xiaoxiao/ChatMessageViewHolder.java
+++ app/src/main/java/org/xuxiaoxiao/www/xiaoxiao/ChatMessageViewHolder.java
@@ -3,16 +3,23 @@ package org.xuxiaoxiao.www.xiaoxiao;
 import android.app.Activity;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+//import static org.xuxiaoxiao.www.xiaoxiao.R.id.author;
 
 /**
  * Created by WuQiang on 2017/3/30.
  */
 
 public class ChatMessageViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-    private TextView mAuthorTextView;
-    private TextView mMessageTextView;
+    private TextView leftMsg;
+    private TextView rightMsg;
+
+    /////////////
+    private LinearLayout leftLayout;
+    private LinearLayout rightLayout;
 
     private ChatMessage mChatMessage;
     private Activity activity;
@@ -20,15 +27,30 @@ public class ChatMessageViewHolder extends RecyclerView.ViewHolder implements Vi
     public ChatMessageViewHolder(View itemView,Activity activity) {
         super(itemView);
         this.activity = activity;
-        mAuthorTextView = (TextView) itemView.findViewById(R.id.author);
-        mMessageTextView = (TextView) itemView.findViewById(R.id.message);
+        leftMsg = (TextView) itemView.findViewById(R.id.left_msg);
+        rightMsg = (TextView) itemView.findViewById(R.id.right_msg);
+
+        leftLayout = (LinearLayout) itemView.findViewById(R.id.left_layout);
+        rightLayout = (LinearLayout) itemView.findViewById(R.id.right_layout);
+
         itemView.setOnClickListener(this);
+        ////////////////////////
+
     }
 
     public void bind(ChatMessage chatmessage) {
         mChatMessage = chatmessage;
-        mAuthorTextView.setText(chatmessage.getAuthor());
-        mMessageTextView.setText(chatmessage.getMessage());
+
+        if (mChatMessage.getAuthor() != null && mChatMessage.getAuthor().equals("WQ")) {
+            rightLayout.setVisibility(View.VISIBLE);
+            leftLayout.setVisibility(View.GONE);
+            rightMsg.setText(mChatMessage.getMessage());
+        } else {
+            // 如果是收到的消息，则显示左边的消息布局，将右边的消息布局隐藏
+            leftLayout.setVisibility(View.VISIBLE);
+            rightLayout.setVisibility(View.GONE);
+            leftMsg.setText(mChatMessage.getMessage());
+        }
 
     }
 
@@ -38,6 +60,26 @@ public class ChatMessageViewHolder extends RecyclerView.ViewHolder implements Vi
 
     }
 }
+
+/**
+ *
+ *     @Override
+protected void populateView(Context context, MessageViewHolder holder, Chat chat) {
+// 设置发送者
+String author = chat.getAuthor();
+// 根据是不是自己发的，改变 发送者 颜色
+if (author != null && author.equals(mUsername)) {
+holder.rightLayout.setVisibility(View.VISIBLE);
+holder.leftLayout.setVisibility(View.GONE);
+holder.rightMsg.setText(chat.getMessage());
+} else {
+// 如果是收到的消息，则显示左边的消息布局，将右边的消息布局隐藏
+holder.leftLayout.setVisibility(View.VISIBLE);
+holder.rightLayout.setVisibility(View.GONE);
+holder.leftMsg.setText(chat.getMessage());
+}
+}
+ */
 /**
  *
  *
