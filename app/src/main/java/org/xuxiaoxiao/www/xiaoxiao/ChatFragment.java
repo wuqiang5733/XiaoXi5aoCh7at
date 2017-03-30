9ac3fed7a3aa86d8936e6aedda2001a8c868ebc3
xiaoqianghou <2662651103@qq.com>
2017-03-30 13:42:33 +0800

    加了RecyclerView与LinearLayoutManager

--- app/src/main/java/org/xuxiaoxiao/www/xiaoxiao/ChatFragment.java
+++ app/src/main/java/org/xuxiaoxiao/www/xiaoxiao/ChatFragment.java
@@ -3,6 +3,8 @@ package org.xuxiaoxiao.www.xiaoxiao;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,7 @@ import android.view.ViewGroup;
  */
 
 public class ChatFragment extends Fragment {
+    private RecyclerView mChatRecyclerView;
     public static ChatFragment newInstance(){
         return new ChatFragment();
     }
@@ -24,6 +27,9 @@ public class ChatFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chat,container,false);
+        View view =  inflater.inflate(R.layout.fragment_chat,container,false);
+        mChatRecyclerView = (RecyclerView) view.findViewById(R.id.chat_recycler_view);
+        mChatRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
     }
 }
