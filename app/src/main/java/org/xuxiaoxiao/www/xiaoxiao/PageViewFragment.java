fd40f8612b22b1e5e142de44fe1bc6a49af3a665
xiaoqianghou <2662651103@qq.com>
2017-04-08 13:37:58 +0800

    Img界面布局也已经好了

--- app/src/main/java/org/xuxiaoxiao/www/xiaoxiao/PageViewFragment.java
+++ app/src/main/java/org/xuxiaoxiao/www/xiaoxiao/PageViewFragment.java
@@ -1,18 +1,8 @@
-/***
- Copyright (c) 2012-14 CommonsWare, LLC
- Licensed under the Apache License, Version 2.0 (the "License"); you may not
- use this file except in compliance with the License. You may obtain a copy
- of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
- by applicable law or agreed to in writing, software distributed under the
- License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
- OF ANY KIND, either express or implied. See the License for the specific
- language governing permissions and limitations under the License.
-
- Covered in detail in the book _The Busy Coder's Guide to Android Development_
- https://commonsware.com/Android
- */
-
 package org.xuxiaoxiao.www.xiaoxiao;
+
+/**
+ * Created by WuQiang on 2017/4/8.
+ */
 
 import android.content.Context;
 import android.os.Bundle;
@@ -64,9 +54,14 @@ public class PageViewFragment extends android.support.v4.app.Fragment {
     }
 
     private class ImgAdapter extends RecyclerView.Adapter<ImgViewHolder> {
+        private int[] temp = new int[12];
+
 
         public ImgAdapter() {
             Log.d("WQ", "ImgAdapter");
+            for(int i = 0; i < 12 ; i ++){
+                temp[i] = i;
+            }
         }
 
         @Override
@@ -78,7 +73,7 @@ public class PageViewFragment extends android.support.v4.app.Fragment {
 
         @Override
         public void onBindViewHolder(ImgViewHolder holder, int position) {
-            holder.mTextView.setText("text");
+            holder.mTextView.setText(String.valueOf(temp[position]));
         }
 
         @Override
